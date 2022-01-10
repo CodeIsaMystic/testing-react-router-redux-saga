0a6214fc297edf6dfd85a54cd2ab9d91a07ae266
Julien Orjollet <oj.professionnel@gmail.com>
2022-01-10 15:36:25 +0100

    first commit

---
+++ base-app/server/README.md
@@ -0,0 +1,16 @@
+# Popular Music Venue Server
+
+### A server for the "Popular Music Venue" app in the Udemy course [Advanced React Testing: Redux Saga and React Router](https://www.udemy.com/course/advanced-react-testing/?referralCode=317FB8E2C866D75D3B71)
+
+### Course Coupons
+See https://bonnie.dev/courses for more information about the instructor and course coupons. 
+
+## Installing
+
+1. Run `npm install`
+2. `cp .env_template .env`
+3. Optional, only necessary if you're going to deploy: update `.env` to contain your own secret string (can just mash the keyboard to generate a long random string)
+
+## Starting the server
+
+Run `npm start`. The server will be found at [http://localhost:3030]
---
+++ base-app/server/src/db-func/shows.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-param-reassign */
+import { Show, ShowWithoutAvailableSeatCount } from '../../../shared/types';
+import { venueCapacity } from '../../db/constants';
+import {
+  filenames,
+  getItemById,
+  getJSONfromFile,
+  writeJSONToFile,
+} from './general';
+import { getAvailableSeatCountByShowId } from './reservations';
+
+export async function writeShows(
+  newShowsArray: ShowWithoutAvailableSeatCount[],
+): Promise<void> {
+  await writeJSONToFile(filenames.shows, newShowsArray);
+}
+
+export async function getShows(): Promise<Show[]> {
+  const showsMinusAvailableSeatCount = await getJSONfromFile<ShowWithoutAvailableSeatCount>(
+    filenames.shows,
+  );
+  const availableSeatCountByShowId = await getAvailableSeatCountByShowId();
+
+  const fullDataShows = showsMinusAvailableSeatCount.map((show) => {
+    const availableSeatCount =
+      availableSeatCountByShowId[show.id] ?? venueCapacity;
+    const fullDataShow = {
+      ...show,
+      availableSeatCount,
+    };
+    return fullDataShow;
+  });
+
+  return fullDataShows;
+}
+
+export async function getShowById(showId: number): Promise<Show> {
+  const showWithoutSeatCount = await getItemById<ShowWithoutAvailableSeatCount>(
+    showId,
+    filenames.shows,
+    'show',
+  );
+  const availableSeatCountByShowId = await getAvailableSeatCountByShowId();
+  const availableSeatCount =
+    availableSeatCountByShowId[showId] ?? venueCapacity;
+  return { ...showWithoutSeatCount, availableSeatCount };
+}
